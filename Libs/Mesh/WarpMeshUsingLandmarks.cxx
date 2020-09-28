36502ed63c0c867d116a077edb339313da73d949
Riddhish Bhalodia <riddhishb@nightfury12.sci.utah.edu>
2020-09-28 14:08:59 -0600

    lib mesh warp

--- Libs/Mesh/WarpMeshUsingLandmarks.cxx
+++ Libs/Mesh/WarpMeshUsingLandmarks.cxx
@@ -19,64 +19,64 @@
 #include <vtkIdList.h>
 #include <vtkIdTypeArray.h>
 
-void point_mesh_squared_distance(
-  const Eigen::PlainObjectBase<DerivedP> & P,
-  const Eigen::PlainObjectBase<DerivedV> & V,
-  const Eigen::MatrixXi & Ele,
-  Eigen::PlainObjectBase<DerivedsqrD> & sqrD,
-  Eigen::PlainObjectBase<DerivedI> & I,
-  Eigen::PlainObjectBase<DerivedC> & C)
-{
-  const size_t dim = P.cols();
-  assert((dim == 2) && "P.cols() should be 2 -- needs a surface mesh");
-  assert(P.cols() == V.cols() && "P.cols() should equal V.cols()");
-    if(dim == 2){
-	  AABB<DerivedV,2> tree;
-      tree.init(V,Ele);
-      return tree.squared_distance(V,Ele,P,sqrD,I,C);
-    }
+// void point_mesh_squared_distance(
+//   const Eigen::PlainObjectBase<DerivedP> & P,
+//   const Eigen::PlainObjectBase<DerivedV> & V,
+//   const Eigen::MatrixXi & Ele,
+//   Eigen::PlainObjectBase<DerivedsqrD> & sqrD,
+//   Eigen::PlainObjectBase<DerivedI> & I,
+//   Eigen::PlainObjectBase<DerivedC> & C)
+// {
+//   const size_t dim = P.cols();
+//   assert((dim == 2) && "P.cols() should be 2 -- needs a surface mesh");
+//   assert(P.cols() == V.cols() && "P.cols() should equal V.cols()");
+//     if(dim == 2){
+// 	  AABB<DerivedV,2> tree;
+//       tree.init(V,Ele);
+//       return tree.squared_distance(V,Ele,P,sqrD,I,C);
+//     }
   
-}
+// }
 ////////////////// AABB IS THIS USED SOMEWHERE ELSE ????
 
-Eigen::MatrixXd W_precomputation(Eigen::MatrixXd Vcontrol_static, Eigen::MatrixXd TV, Eigen::MatrixXi TF){ 
+// Eigen::MatrixXd W_precomputation(Eigen::MatrixXd Vcontrol_static, Eigen::MatrixXd TV, Eigen::MatrixXi TF){ 
 
-    vector<Eigen::MatrixXd> v;
-    Eigen::MatrixXd W;
-    Eigen::VectorXi b;
-    {
-      Eigen::VectorXi J = Eigen::VectorXi::LinSpaced(TV.rows(),0,TV.rows()-1);
-      Eigen::VectorXd sqrD;
-      Eigen::MatrixXd _2;
-      std::cout<<"Finding closest points..."<<std::endl;
-      igl::point_mesh_squared_distance(Vcontrol_static,TV,J,sqrD,b,_2);
-      assert(sqrD.minCoeff() < 1e-7 && "low.V should exist in high.V");
-    }
-    // force perfect positioning, rather have popping in low-res than high-res.
-    // The correct/elaborate thing to do is express original low.V in terms of
-    // linear interpolation (or extrapolation) via elements in (high.V,high.F)
-    igl::slice(TV,b,1,Vcontrol_static);
-    // list of points --> list of singleton lists
-    std::vector<std::vector<int> > S;
-    igl::matrix_to_list(b,S);
-    std::cout<<"Computing weights for "<<b.size()<<
-      " handles at "<<TV.rows()<<" vertices..."<<std::endl;
-    const int k = 2;
-    igl::biharmonic_coordinates(TV,TF,S,k,W);
-    std::cout<<"Reindexing..."<< std::endl;
-    std::cout << W.rows() << " " << W.cols() << std::endl;
-    // Throw away interior tet-vertices, keep weights and indices of boundary
-    Eigen::VectorXi I,J;
-    igl::remove_unreferenced(TV.rows(),TF,I,J);
-    for_each(TF.data(),TF.data()+TF.size(),[&I](int & a){a=I(a);});
-    for_each(b.data(),b.data()+b.size(),[&I](int & a){a=I(a);});
-    igl::slice(Eigen::MatrixXd(TV),J,1,TV);
-    igl::slice(Eigen::MatrixXd(W),J,1,W);
-    std::cout << "It's done!!" << std::endl;
-    // v.push_back(W);
-    // v.push_back(Vcontrol_static);
-    return W;
-}
+//     vector<Eigen::MatrixXd> v;
+//     Eigen::MatrixXd W;
+//     Eigen::VectorXi b;
+//     {
+//       Eigen::VectorXi J = Eigen::VectorXi::LinSpaced(TV.rows(),0,TV.rows()-1);
+//       Eigen::VectorXd sqrD;
+//       Eigen::MatrixXd _2;
+//       std::cout<<"Finding closest points..."<<std::endl;
+//       igl::point_mesh_squared_distance(Vcontrol_static,TV,J,sqrD,b,_2);
+//       assert(sqrD.minCoeff() < 1e-7 && "low.V should exist in high.V");
+//     }
+//     // force perfect positioning, rather have popping in low-res than high-res.
+//     // The correct/elaborate thing to do is express original low.V in terms of
+//     // linear interpolation (or extrapolation) via elements in (high.V,high.F)
+//     igl::slice(TV,b,1,Vcontrol_static);
+//     // list of points --> list of singleton lists
+//     std::vector<std::vector<int> > S;
+//     igl::matrix_to_list(b,S);
+//     std::cout<<"Computing weights for "<<b.size()<<
+//       " handles at "<<TV.rows()<<" vertices..."<<std::endl;
+//     const int k = 2;
+//     igl::biharmonic_coordinates(TV,TF,S,k,W);
+//     std::cout<<"Reindexing..."<< std::endl;
+//     std::cout << W.rows() << " " << W.cols() << std::endl;
+//     // Throw away interior tet-vertices, keep weights and indices of boundary
+//     Eigen::VectorXi I,J;
+//     igl::remove_unreferenced(TV.rows(),TF,I,J);
+//     std::for_each(TF.data(),TF.data()+TF.size(),[&I](int & a){a=I(a);});
+//     std::for_each(b.data(),b.data()+b.size(),[&I](int & a){a=I(a);});
+//     igl::slice(Eigen::MatrixXd(TV),J,1,TV);
+//     igl::slice(Eigen::MatrixXd(W),J,1,W);
+//     std::cout << "It's done!!" << std::endl;
+//     // v.push_back(W);
+//     // v.push_back(Vcontrol_static);
+//     return W;
+// }
 
 Eigen::MatrixXd pointReadFormat(std::string refPointPath, int numP){
   Eigen::MatrixXd Vout(numP, 3);
